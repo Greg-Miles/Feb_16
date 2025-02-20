73adb6144968504472059846b451e380d6539440
Greg <sogmeadmug@gmail.com>
2025-02-20 20:58:20 +0300

    валидация

--- citiesiterator.py
+++ citiesiterator.py
@@ -1,21 +1,35 @@
 import json
-from dataclasses import dataclass, field
+from dataclasses import dataclass, fields
 from typing import Self, List, Dict
 
 @dataclass
 class City:
-    lat: float
-    lon: float
+    coords: Dict[str, str]
     district: str
     name: str
     population: int
     subject: str
 
+    @classmethod
+    def validate_city_data(cls, city_data: dict) -> bool:
+        required_fields = {field.name for field in fields(cls)}
+        provided_fields = set(city_data.keys())
+        return required_fields == provided_fields
+
 class CitiesIterator:
 
     def __init__(self, filepath: str):
-        with open(filepath, "r")as file:
-            self.cities = [City(**city) for city in json.load(file)]
+
+        with open(filepath, "r", encoding="utf-8-sig")as file:
+            cities_data = json.load(file)
+            self.cities = []
+            for city in cities_data:
+                if City.validate_city_data(city):
+                    self.cities.append(City(**city))
+                else:
+                    raise ValueError("Неверный формат данных json-файла")
+
+
         self.index = 0
 
     def __iter__(self)-> Self:
@@ -27,3 +41,7 @@ class CitiesIterator:
         city = self.cities[self.index]
         self.index += 1
         return city
+
+c_iter = CitiesIterator("cities.json")
+print(next(c_iter))
+print(next(c_iter))
