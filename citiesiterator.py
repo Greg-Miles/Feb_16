bc8c20ba8eaa8a55f873f55eb1e91136e79fff04
Greg <sogmeadmug@gmail.com>
2025-02-20 21:55:03 +0300

    методы для сортировки и фильтрации

--- citiesiterator.py
+++ citiesiterator.py
@@ -1,6 +1,7 @@
 import json
 from dataclasses import dataclass, fields
 from typing import Self, List, Dict
+from pprint import pprint
 
 @dataclass
 class City:
@@ -22,14 +23,14 @@ class CitiesIterator:
 
         with open(filepath, "r", encoding="utf-8-sig")as file:
             cities_data = json.load(file)
-            self.cities = []
+            self.all_cities = []
             for city in cities_data:
                 if City.validate_city_data(city):
-                    self.cities.append(City(**city))
+                    self.all_cities.append(City(**city))
                 else:
                     raise ValueError("Неверный формат данных json-файла")
 
-
+        self.cities = self.all_cities.copy()
         self.index = 0
 
     def __iter__(self)-> Self:
@@ -41,7 +42,26 @@ class CitiesIterator:
         city = self.cities[self.index]
         self.index += 1
         return city
+    
+    def set_population_filter(self, min_pop: int = 1, max_pop: int = 20_000_000)-> Self:
+        self.cities = [city for city in self.all_cities if min_pop <= city.population <= max_pop]
+        self.index = 0
+        return self
+
+    def sort_by_parameter(self, parameter: str, reverse: bool = False) -> Self:
+        self.cities.sort(key=lambda city: getattr(city, parameter), reverse=reverse)
+        self.index = 0
+        return self
+
+
 
 c_iter = CitiesIterator("cities.json")
+
+c_iter.set_population_filter(10000, 20000)
 print(next(c_iter))
 print(next(c_iter))
+c_iter.set_population_filter(20000, 22000)
+c_iter.sort_by_parameter("district")
+pprint(list(c_iter), sort_dicts=False)
+c_iter.sort_by_parameter("subject")
+pprint(list(c_iter), sort_dicts=False)
